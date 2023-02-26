6ba825c0021eed3d84b17719a3be247dced8c0cd
JohnAndrewsAsare <asarejohnandrews@gmail.com>
2023-02-26 14:07:49 +0000

    fixed resume download issue

--- components/Navbar.js
+++ components/Navbar.js
@@ -18,8 +18,8 @@ const Navbar = () => {
           <li>
             <a
               className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-              href="../assets/Resume.pdf"
-              download
+              href="/Resume.pdf"
+              download="Resume"
             >
               Resume
             </a>
